ffd0f646b261275698b515c446352c40c61b2295
labst <labst925@gmail.com>
2021-11-30 18:47:06 +0900

    first commit

---
+++ ezenGit/BeatCoding/src/Problem167.java
@@ -0,0 +1,15 @@
+
+public class Problem167 {
+
+	public static void main(String[] args) {
+		for(int i=0; i<=10; i++) {
+			for(int j=0;j<=10;j++) {
+				if(3*i+2*j==10) {
+					System.out.println(i+", "+j);
+				}
+			}
+		}
+
+	}
+
+}
